647f46dacf0debebe07575d505527673b3799627
Ivander Carlos Dias <ivandercarlos@gmail.com>
2021-07-09 14:52:49 -0300

    Get navigator geolocation

--- script.js
+++ script.js
@@ -2,3 +2,17 @@
 
 const countriesContainer = document.querySelector('.countries')
 const btn = document.querySelector('.btn-country')
+
+const getPosition = function () {
+    return new Promise((resolve, reject) => {
+        // navigator.geolocation.getCurrentPosition(
+        //     (position) => resolve(position),
+        //     (err) => reject(err)
+        // )
+        navigator.geolocation.getCurrentPosition(resolve, reject)
+    })
+}
+
+getPosition()
+    .then((position) => console.log(position))
+    .catch((err) => console.error(err.message))
